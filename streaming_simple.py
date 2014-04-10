93b4ec35a1688af06d71c3e2028dc9c360fd4607
Vukosi Marivate <vimarambo@gmail.com>
2014-04-10 13:37:46 -0400

    Formatting for Pycharms + small edits for personal use

--- streaming_simple.py
+++ streaming_simple.py
@@ -13,48 +13,48 @@ from auth import TwitterAuth
 #Text of tweets also printed as recieved (see note about not doing this in production (final) code
 
 class StdOutListener(StreamListener):
-	
-	#This function gets called every time a new tweet is received on the stream
-	def on_data(self, data):
-		#Just write data to one line in the file
-		fhOut.write(data)
-		
-		#Convert the data to a json object (shouldn't do this in production; might slow down and miss tweets)
-		j=json.loads(data)
+    #This function gets called every time a new tweet is received on the stream
+    def on_data(self, data):
+        #Just write data to one line in the file
+        fhOut.write(data)
 
-		#See Twitter reference for what fields are included -- https://dev.twitter.com/docs/platform-objects/tweets
-		text=j["text"] #The text of the tweet
-		print(text) #Print it out
+        #Convert the data to a json object (shouldn't do this in production; might slow down and miss tweets)
+        j = json.loads(data)
 
-	def on_error(self, status):
-		print("ERROR")
-		print(status)
+        #See Twitter reference for what fields are included -- https://dev.twitter.com/docs/platform-objects/tweets
+        text = j["text"]  #The text of the tweet
+        print(text)  #Print it out
+
+    def on_error(self, status):
+        print("ERROR")
+        print(status)
+
 
 if __name__ == '__main__':
-	try:
-		#Create a file to store output. "a" means append (add on to previous file)
-		fhOut = open("output.json","a")
+    try:
+        #Create a file to store output. "a" means append (add on to previous file)
+        fhOut = open("output.json", "a")
 
-		#Create the listener
-		l = StdOutListener()
-		auth = OAuthHandler(TwitterAuth.consumer_key, TwitterAuth.consumer_secret)
-		auth.set_access_token(TwitterAuth.access_token, TwitterAuth.access_token_secret)
+        #Create the listener
+        l = StdOutListener()
+        auth = OAuthHandler(TwitterAuth.consumer_key, TwitterAuth.consumer_secret)
+        auth.set_access_token(TwitterAuth.access_token, TwitterAuth.access_token_secret)
 
-		#Connect to the Twitter stream
-		stream = Stream(auth, l)	
+        #Connect to the Twitter stream
+        stream = Stream(auth, l)
 
-		#Terms to track
-		stream.filter(track=["oxford","london","wolverhampton"])
-		
-		#Alternatively, location box  for geotagged tweets
-		#stream.filter(locations=[-0.530, 51.322, 0.231, 51.707])
+        #Terms to track
+        stream.filter(track=["malema", "zuma", "zille", "ramphele"])
 
-	except KeyboardInterrupt:
-		#User pressed ctrl+c -- get ready to exit the program
-		pass
+    #Alternatively, location box  for geotagged tweets
+    #stream.filter(locations=[-0.530, 51.322, 0.231, 51.707])
 
-	#Close the 
-	fhOut.close()
+    except KeyboardInterrupt:
+        #User pressed ctrl+c -- get ready to exit the program
+        pass
+
+    #Close the
+    fhOut.close()
 
 
 
